91145875f2e9960754d8a725d08d030ec03369f9
dina lim <dina.lim14@gmail.com>
2024-03-20 11:53:29 +0100

    Ajout de la section Mes réalisations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 
 <script setup>
 import About from '@/components/About.vue';
-import Skills from '@/components/Skills.vue'
+import Skills from '@/components/Skills.vue';
+import Creations from '@/components/Creations.vue'
 </script>
 
 <template>
   <main>
-<About />
-<Skills />
+    <About />
+    <Skills />
+    <Creations />
   </main>
 </template>
 
