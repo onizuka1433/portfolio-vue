4c027e0921c93fb9d4b687cdbaedecab57783145
dina lim <dina.lim14@gmail.com>
2024-03-28 11:10:58 +0100

    Ajout du menu nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,25 @@ import background from "@/assets/images/background.jpg"
 </script>
 
 <template>
-    <div class="slider">
-      <img :src="background" class="slider-background" />
+    <div>
+      <nav class="navbar">
+        <ul>
+          <li><a href="#about">À propos</a></li>
+          <li><a href="#skills">Compétences</a></li>
+          <li><a href="#creations">Réalisations</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+  
+      <div class="slider">
+        <img :src="background" class="slider-background" />
         <div class="slider-content">
-          <h1> Dina LIM
+          <h1>Dina LIM</h1>
           <p>Développeur full-stack junior</p>
-          </h1>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 
@@ -19,6 +29,32 @@ import background from "@/assets/images/background.jpg"
 </script>
 
 <style scoped>
+.navbar {
+  background-color: #333;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar ul li {
+  display: inline;
+
+}
+
+.navbar ul li a {
+  display: inline-block;
+  padding: 10px 20px;
+  color: #fff;
+  text-decoration: none;
+}
+
+.navbar ul li a:hover {
+  background-color: #555;
+}
+
 .slider{
     width:100%;
     min-height:80vh;
