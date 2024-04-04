403eb1602bf4284aedfcca96f94f2432cf9af723
dina lim <dina.lim14@gmail.com>
2024-04-04 12:29:10 +0200

    Add style nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
 
 
 </script>
 
 <template>
   <Header />
-  <Footer />
+
 
   <RouterView />
 </template>
